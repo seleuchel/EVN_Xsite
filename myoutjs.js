8f3378b03eb78dcaf99f8515635cb6606cbb81e7
seleuchel <37205198+seleuchel@users.noreply.github.com>
2023-02-07 03:37:49 +0900

    Update myoutjs.js

--- myoutjs.js
+++ myoutjs.js
@@ -1,1 +1,20 @@
 
+window.onload = function(){
+
+
+
+
+	var h1 = document.getElementById('hello45444443');
+
+
+  	h1.addEventListener('click', function () {
+
+
+		alert('Hello3, world');
+
+  })
+
+
+};
+
+
